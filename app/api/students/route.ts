4977a669ef2f58cd6d07ac146b1baf01e1910927
meuorii <fontillaskian@gmail.com>
2026-01-05 23:22:27 +0800

    implement student creation API with validation and duplicate checks

--- app/api/students/route.ts
+++ app/api/students/route.ts
@@ -11,3 +11,25 @@ export async function GET() {
         return NextResponse.json({ error: 'Failed to fetch students' }, { status: 500 })
     } 
 }
+
+export async function POST(req: Request) {
+    try {
+        await dbConnect();
+        const { studentId, fullName } = await req.json();
+
+        if (!fullName || !studentId) {
+            return NextResponse.json({ error: 'Both Full Name and Student ID are required' }, { status: 400 });
+        }
+
+        const existingStudent = await Student.findOne({ studentId });
+        if (existingStudent) {
+            return NextResponse.json({ error: 'This Student ID already exists' }, { status: 400 });
+        }
+
+        const newStudent = new Student({ studentId, fullName });
+        await newStudent.save();
+        return NextResponse.json({ message: 'Student added successfully', data: newStudent }, { status: 201 });
+    } catch {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500})
+    }
+}
