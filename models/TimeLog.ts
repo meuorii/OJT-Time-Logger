22bbee8474f07940a719cc97ffd64367aa2929d2
meuorii <fontillaskian@gmail.com>
2026-01-08 23:22:26 +0800

    update TimeLog Schema with AM/PM/OT slots and auto computed fields

--- models/TimeLog.ts
+++ models/TimeLog.ts
@@ -1,11 +1,18 @@
 import { Schema, model, models } from  'mongoose';
 
 const TimeLogSchema = new Schema ({
+    student: { type: Schema.Types.ObjectId, ref: 'Studeunt', required: true },
     studentId: { type: String, required: true },
     fullName: { type: String, required: true },
-    date: { type: String, required: true },
-    timeIn: { type: Date },
-    timeOut: { type: Date},
-})
+    date: { tyoe: String, required: true},
+    amIn: { type: String, default: null },
+    amOut: { type: String, default: null },
+    pmIn: { type: String, default: null },
+    pmOut: { type: String, default: null},
+    otIn: { type: String, default: null },
+    otOut: { type: String, default: null },
+    totalHours: { type: Number, default: 0},
+    status: { type: String, enum: ['Complete', 'Incomplete']},
+}, { timestamps: true })
 
 export const TimeLog = models.TimeLog || model('TimeLog', TimeLogSchema);
